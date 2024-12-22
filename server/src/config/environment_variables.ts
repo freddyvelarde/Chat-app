6bd82a4e1b876641bceddd0b6f297ff7e41d9ebe
freddyvelarde <freddvel.dev@gmail.com>
2024-12-21 22:18:13 -0400

    adding routes and controllers for users and authentication.

--- server/src/config/environment_variables.ts
+++ server/src/config/environment_variables.ts
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
-const { HOST_PORT } = process.env;
+const { HOST_PORT, SECRET_KEY_JWT } = process.env;
 
 const PORT = HOST_PORT || 7575;
+const SECRET_KEY_JWT_VAR = SECRET_KEY_JWT || "secret_key :D";
 
-export { PORT };
+export { PORT, SECRET_KEY_JWT_VAR };
