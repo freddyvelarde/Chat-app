7c78f7f25dc83557585207a89dc61b276e970f65
freddyvelarde <freddvel.dev@gmail.com>
2024-12-22 15:59:05 -0400

    adding `react-router`

---
+++ webClient/src/pages/LogIn/LogIn.tsx
@@ -0,0 +1,64 @@
+import { ChangeEvent, SyntheticEvent, useState } from "react";
+import { logInUser } from "../../config/endpoints";
+
+interface IForm {
+  username: string;
+  password: string;
+}
+
+const LogIn = () => {
+  const [form, setForm] = useState<IForm>({ username: "", password: "" });
+  const [response, setResponse] = useState<any>(null);
+
+  const createNewUser = async ({ username, password }: IForm) => {
+    const req = await fetch(logInUser, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    const res = await req.json();
+    setResponse(res);
+  };
+
+  const handleOnChangeEventUsername = (e: ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, username: e.target.value });
+  };
+  const handleOnChangeEventPassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, password: e.target.value });
+  };
+
+  const handleFormEvent = (e: SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    createNewUser(form);
+    console.log(response);
+  };
+
+  return (
+    <form action="" onSubmit={handleFormEvent}>
+      <div>
+        <label htmlFor="">Username:</label>
+        <input
+          type="text"
+          value={form.username}
+          onChange={handleOnChangeEventUsername}
+          placeholder="Type your username"
+        />
+      </div>
+      <div>
+        <label htmlFor="">Password:</label>
+        <input
+          type="password"
+          onChange={handleOnChangeEventPassword}
+          value={form.password}
+          placeholder="Type your password"
+        />
+      </div>
+      <button>Create</button>
+      <p>{response ? response.message : null}</p>
+    </form>
+  );
+};
+
+export default LogIn;
