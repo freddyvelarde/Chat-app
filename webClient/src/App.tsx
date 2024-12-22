3e07cf46c34f81f26f4d48aed69571b6cc1695af
freddyvelarde <freddvel.dev@gmail.com>
2024-12-21 23:07:15 -0400

    adding `tailwindcss` and `react router`

--- webClient/src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h1>Starting the web client project</h1>
-    </>
-  );
-}
-
-export default App;
---
+++ webClient/src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <>
+      <h1 className="text-3xl text-gray-800 font-bold ">Hello world!</h1>
+    </>
+  );
+};
+
+export default Home;
